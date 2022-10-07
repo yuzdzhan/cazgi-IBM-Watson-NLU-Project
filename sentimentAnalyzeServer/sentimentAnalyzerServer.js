6e95699bb6b7b4dc44502af49043fe459075a4cf
Yuzdzhan Mehmedov <ynmehmedov@gmail.com>
2022-10-07 13:48:47 -0400

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -83,10 +83,11 @@ app.get("/url/sentiment", (req,res) => {
         naturalLanguageUnderstanding.analyze(analyzeParams)
         .then(analysisResults => {
             //Retrieve the sentiment and return it as a formatted string
-
+console.log(analysisResults);
             return res.send(analysisResults.result.keywords[0].sentiment,null,2);
         })
         .catch(err => {
+            console.log(err);
             return res.send("Could not do desired operation "+err);
         });
 });
